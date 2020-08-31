53da8301f96851e5f0a332f9118a764cb32114c0
pika <pika@pikapika.ninja>
2020-08-31 01:09:52 -0700

    refactor digit converting utility methods to DigitUtilities

--- src/main/java/com/fama/numtoenglish/translator/utilities/TranslatorUtilities.java
+++ src/main/java/com/fama/numtoenglish/translator/utilities/TranslatorUtilities.java
@@ -32,7 +32,7 @@ public final class TranslatorUtilities {
                     if (builder.length() > 0) {
                         builder.insert(0, " ");
                     }
-                    builder.insert(0, powerOfThousandToName(count));
+                    builder.insert(0, DigitUtilities.powerOfThousandToName(count));
                     builder.insert(0, " ");
                 }
                 builder.insert(0, threeDigitsName);
@@ -48,13 +48,13 @@ public final class TranslatorUtilities {
             builder.append(" point");
             for (int i = decimalPtIndex + 1; i < number.length(); ++i) {
                 builder.append(" ");
-                builder.append(digitToName("" + number.charAt(i)));
+                builder.append(DigitUtilities.digitToName("" + number.charAt(i)));
             }
         }
         return builder.toString();
     }
 
-    protected static String threeDigitNumberToName(String number) {
+    static String threeDigitNumberToName(String number) {
         String threeDigitNumber = number;
         if (number.length() == 1) {
             threeDigitNumber = "00" + number;
@@ -67,7 +67,7 @@ public final class TranslatorUtilities {
         // hundreds place is represented by "x hundred"
         String hundredsDigit = threeDigitNumber.substring(0, 1);
         if (!"0".equals(hundredsDigit)) {
-            nameParts.add(digitToName(hundredsDigit));
+            nameParts.add(DigitUtilities.digitToName(hundredsDigit));
             nameParts.add("hundred");
         }
 
@@ -77,144 +77,20 @@ public final class TranslatorUtilities {
         boolean skipOnesDigit = false;
         if (!tensDigit.equals("0")) {
             if ("1".equals(tensDigit)) {
-                nameParts.add(teensNumberToName(teenDigits));
+                nameParts.add(DigitUtilities.teensNumberToName(teenDigits));
                 skipOnesDigit = true;
             } else {
-                nameParts.add(tensDigitToName(tensDigit));
+                nameParts.add(DigitUtilities.tensDigitToName(tensDigit));
             }
         }
 
         // ones place is present in the name if the tens place is not 1
         String onesDigit = threeDigitNumber.substring(2, 3);
         if (!skipOnesDigit && !"0".equals(onesDigit)) {
-            nameParts.add(digitToName(onesDigit));
+            nameParts.add(DigitUtilities.digitToName(onesDigit));
         }
 
         return String.join(" ", nameParts);
     }
 
-    private static String powerOfThousandToName(int power) {
-        // names taken from Powers of 10 Wikipedia page: https://en.wikipedia.org/wiki/Power_of_10
-        switch (power) {
-            case 1:
-                return "thousand";
-            case 2:
-                return "million";
-            case 3:
-                return "billion";
-            case 4:
-                return "trillion";
-            case 5:
-                return "quadrillion";
-            case 6:
-                return "quintillion";
-            case 7:
-                return "sextillion";
-            case 8:
-                return "septillion";
-            case 9:
-                return "octillion";
-            case 10:
-                return "nonillion";
-            case 11:
-                return "decillion";
-            case 12:
-                return "undecillion";
-            case 13:
-                return "duidecillion";
-            case 14:
-                return "tredecillion";
-            case 15:
-                return "quattuordecillion";
-            case 16:
-                return "quindecillion";
-            case 17:
-                return "sexdecillion";
-            case 18:
-                return "septdecillion";
-            case 19:
-                return "octodecillion";
-            case 20:
-                return "novemdecillion";
-            case 21:
-                return "vigintillion";
-        }
-        return "";
-    }
-
-    private static String teensNumberToName(String teen) {
-        switch (teen) {
-            case "10":
-                return "ten";
-            case "11":
-                return "eleven";
-            case "12":
-                return "twelve";
-            case "13":
-                return "thirteen";
-            case "14":
-                return "fourteen";
-            case "15":
-                return "fifteen";
-            case "16":
-                return "sixteen";
-            case "17":
-                return "seventeen";
-            case "18":
-                return "eighteen";
-            case "19":
-                return "nineteen";
-        }
-        return null;
-    }
-
-    private static String tensDigitToName(String digit) {
-        switch (digit) {
-            case "1":
-                return "ten";
-            case "2":
-                return "twenty";
-            case "3":
-                return "thirty";
-            case "4":
-                return "forty";
-            case "5":
-                return "fifty";
-            case "6":
-                return "sixty";
-            case "7":
-                return "seventy";
-            case "8":
-                return "eighty";
-            case "9":
-                return "ninety";
-        }
-        return null;
-    }
-
-    private static String digitToName(String digit) {
-        switch (digit) {
-            case "0":
-                return "zero";
-            case "1":
-                return "one";
-            case "2":
-                return "two";
-            case "3":
-                return "three";
-            case "4":
-                return "four";
-            case "5":
-                return "five";
-            case "6":
-                return "six";
-            case "7":
-                return "seven";
-            case "8":
-                return "eight";
-            case "9":
-                return "nine";
-        }
-        return null;
-    }
 }
